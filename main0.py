6b5afae08c3987e64d4307608596fd56914b2b65
1h3gamer <Idaune.nathan@gmail.com>
2026-01-24 17:59:28 +0000

    sort by height rearranged

--- main0.py
+++ main0.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 data_type = [('name','S15'),('class',int),('height',float)]
-students_details = [('James',5,48.5),('Nail',6,52,5),('Paul',5,42.10),('Pit',4,40.11)]
+students_details = [('James',5,48.5),('Nail',6,52.5),('Paul',5,42.10),('Pit',4,40.11)]
 students = np.array(students_details,dtype=data_type)
 print("original array")
 print(students)
