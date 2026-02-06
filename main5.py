3ac2a32a7507e1c6f1fe145bc75d9e64953e0ff5
1h3gamer <Idaune.nathan@gmail.com>
2026-02-06 17:34:14 +0000

    seaborn intro

---
+++ main5.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("USA_Housing.csv")
+
+print(df.head(10))
+
+print(df.info())
+
+print(df.describe())
+
+print(df.columns)
+
+sns.pairplot(df)
+
+numeric_df = df.select_dtypes(include=[np.number])
+
+plt.figure(figsize=(10,6))
+sns.heatmap(numeric_df.corr(),annot=True,cmap="coolwarm")
+
+plt.show()
